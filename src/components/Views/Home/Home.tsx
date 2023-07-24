21827921e360b7aa9e447734e989f2bf66efccdb
pfsamaja <pfsamaja@uc.cl>
2023-07-24 15:16:01 -0400

    read excel completed

--- src/components/Views/Home/Home.tsx
+++ src/components/Views/Home/Home.tsx
@@ -11,6 +11,7 @@ const Home: React.FC = () => {
         e.target.value = "";
       })
       .catch((err: any) => {
+        alert(err);
         e.target.value = "";
       });
   }
--- src/utils/readExcel.js
+++ src/utils/readExcel.js
@@ -12,12 +12,71 @@ export async function readExcel(file) {
       wb.xlsx
         .load(buffer)
         .then((workbook) => {
+          var finished = false;
           workbook.worksheets[0].eachRow((row, rowNumber) => {
-            if (rowNumber > 1) {
+            if (rowNumber > 1 && !finished) {
+              if (row.getCell(1).value === "Creadas") {
+                finished = true;
+                return;
+              }
               let rowToArray = {};
               rowToArray[headers[0]] = row.getCell(1).value;
-              rowToArray[headers[1]] = row.getCell(11).value;
-              rowToArray[headers[2]] = row.getCell(70).value;
+
+              switch (row.getCell(10).value) {
+                case "1":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "2":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "3":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "4":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "4.05":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "6":
+                  rowToArray[headers[1]] = "completed";
+                  break;
+                case "8":
+                  rowToArray[headers[1]] = "failed";
+                  break;
+                case "21":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "10":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "12":
+                  rowToArray[headers[1]] = "completed";
+                  break;
+                case "16":
+                  rowToArray[headers[1]] = "failed";
+                  break;
+                case "22":
+                  rowToArray[headers[1]] = "pending";
+                  break;
+                case "14":
+                  rowToArray[headers[1]] = "failed";
+                  break;
+                case "14.1":
+                  rowToArray[headers[1]] = "failed";
+                  break;
+                case "14.2":
+                  rowToArray[headers[1]] = "failed";
+                  break;
+                default:
+                  reject("Estado no encontrado en la fila " + rowNumber);
+                  break;
+              }
+              if (row.getCell(10).value === 12) {
+                rowToArray[headers[2]] = row.getCell(84).value;
+              } else {
+                rowToArray[headers[2]] = row.getCell(70).value;
+              }
               rowsToArray.push(rowToArray);
             }
           });
